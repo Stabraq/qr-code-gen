93822aa9f4fe3749bdc0bcfd557125342326f5fd
mero2online <mero2online@gmail.com>
2021-10-21 09:54:24 +0200

    Initial Files

---
+++ src/components/InputField.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+export const InputField = ({
+  name,
+  type,
+  handleChange,
+  min,
+  max,
+  step,
+  defaultValue,
+  hideLabel,
+  value,
+}) => {
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', marginBottom: '6px' }}
+    >
+      {!hideLabel && <label>{name}</label>}
+      <input
+        type={type}
+        id={name}
+        name={name}
+        onChange={handleChange}
+        min={min}
+        max={max}
+        step={step || 1}
+        defaultValue={defaultValue}
+        value={value}
+      />
+    </div>
+  );
+};
---
+++ src/components/SelectField.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+export const SelectField = ({ name, options, handleChange }) => {
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', marginBottom: '6px' }}
+    >
+      <label>{name}</label>
+      <select name={name} onChange={handleChange}>
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
